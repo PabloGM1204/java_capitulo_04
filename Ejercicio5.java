d5a9ce0793468447e1bb5b8c5d3e4fd1f3309d27
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-17 10:18:40 +0200

    Ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ejercicio5{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa resuelve ecucaciones de primer grado del tipo ax+b=0");
+        System.out.print("Dime el valor de a: ");
+        double a = sc.nextDouble();
+        System.out.print("Dime el valor de b: ");
+        double b = sc.nextDouble();
+        
+    }
+}
