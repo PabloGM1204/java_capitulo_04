a5fda893385dcd89e927d02c7a6bddd8f96ed727
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-19 12:03:33 +0200

    Ejercicio14

---
+++ Ejercicio14.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ejercicio14{
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa dice si es un numero es par y si es divisible entre 5");
+        System.out.print("Dime un número: ");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("Su número es par");
+        }else{
+            System.out.println("Su número es impar");
+        }
+
+        if(n%5==0){
+            System.out.println("Su número es divisible entre 5");
+        }else{
+            System.out.println("Su número no es divisible entre 5");
+        }
+    }
+}
