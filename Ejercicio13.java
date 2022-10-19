0deda03c7f36715564efac469c5b617a176d0407
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-19 11:55:41 +0200

    Ejercicio13

---
+++ Ejercicio13.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Ejercicio13{
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("Este programa ordena tres números introducidos");
+        System.out.println("Escribe los tres números para ordenarlos");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+
+        if (n1>n2){
+            a = n1;
+            n1 = n2;
+            n2 = a;
+        }
+
+        if(n2>n3){
+            a = n2;
+            n2 = n3;
+            n3 = a;
+        }
+
+        if(n1>n2){
+            a = n1;
+            n1 = n2;
+            n2 = a;
+        }
+
+        System.out.println("Los numeros ordenador de menor a mayor son: "+n1+" "+n2+" "+n3);
+
+    }
+}
